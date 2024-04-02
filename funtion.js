d30a2b1f04ceedf4e1c206085773c515b0edf390
MonifaAnna <monifamajed21@gmail.com>
2024-04-02 16:52:01 +0600

    first commit

---
+++ funtion.js
@@ -0,0 +1,17 @@
+// ----------carsor
+const cursor = document.querySelector(".cursor");
+document.addEventListener("mousemove",(e) => {
+    let x = e.pageX;
+    let y = e.pageY;
+
+    cursor.style.top = y + "px"
+    cursor.style.left = x + "px"
+    cursor.style.display = "block";
+
+    function mouseStopped(){
+        cursor.style.display ="none";
+    }
+    clearTimeout(timeout);
+    timeout = setTimeout(mouseStopped,1000);
+})
+
