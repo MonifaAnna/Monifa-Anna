1dd780b12f72029ac483220edf3a9f9fff047084
MonifaAnna <monifamajed21@gmail.com>
2024-04-04 04:57:10 +0600

    done

--- funtion.js
+++ funtion.js
@@ -1,38 +1,38 @@
-// ----------carsor
-const cursor = document.querySelector(".cursor");
-document.addEventListener("mousemove",(e) => {
-    let x = e.pageX;
-    let y = e.pageY;
+    // ----------carsor
+    const cursor = document.querySelector(".cursor");
+    document.addEventListener("mousemove",(e) => {
+        let x = e.pageX;
+        let y = e.pageY;
 
-    cursor.style.top = y + "px"
-    cursor.style.left = x + "px"
-    cursor.style.display = "block";
+        cursor.style.top = y + "px"
+        cursor.style.left = x + "px"
+        cursor.style.display = "block";
 
-    function mouseStopped(){
-        cursor.style.display ="none";
-    }
-    clearTimeout(timeout);
-    timeout = setTimeout(mouseStopped,1000);
-})
-// -------------------
-const carsor = document.querySelector(".carsor");
-document.addEventListener("mousemove",(e) => {
-    let x = e.pageX;
-    let y = e.pageY;
+        function mouseStopped(){
+            cursor.style.display ="none";
+        }
+        clearTimeout(timeout);
+        timeout = setTimeout(mouseStopped,1000);
+    })
+    // -------------------
+    const carsor = document.querySelector(".carsor");
+    document.addEventListener("mousemove",(e) => {
+        let x = e.pageX;
+        let y = e.pageY;
 
-    carsor.style.top = y + "px"
-    carsor.style.left = x + "px"
-    carsor.style.display = "block";
+        carsor.style.top = y + "px"
+        carsor.style.left = x + "px"
+        carsor.style.display = "block";
 
-    function mouseStopped(){
-        carsor.style.display ="none";
-    }
-    clearTimeout(timeout);
-    timeout = setTimeout(mouseStopped,1000);
-})
-// -------------preloader------------
-var loader = document.getElementById("preloader")
+        function mouseStopped(){
+            carsor.style.display ="none";
+        }
+        clearTimeout(timeout);
+        timeout = setTimeout(mouseStopped,1000);
+    })
+    // -------------preloader------------
+    var loader = document.getElementById("preloader")
 
-window.addEventListener("load",function(){
-    loader.style.display = "none"
-})
+    window.addEventListener("load",function(){
+        loader.style.display = "none"
+    })
